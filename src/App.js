c2e177e530e9f7c826032e3008c5a303065ea82f
Akhil Sarikonda <akhilascoder@gmail.com>
2019-08-31 20:21:39 +0530

    Added some components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Grid} from '@material-UI/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import youtube from '../src/api/youtube';
+
+class App extends React.Component{
+    render(){
+        return(
+            <Grid justify="center" container spacing={16}>
+                <Grid item xs={12}>
+                    <Grid container spacing={16}>
+                        <Grid item xs={12}>
+                            {/* {Search bar} */}
+                        </Grid>
+
+                        <Grid item xs={8}>
+                            {/* {Video details} */}
+                        </Grid>
+
+                        <Grid item xs={4}>
+                            {/* {video list} */}
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        )
+    }
+};
+
 
 export default App;
+
+
