10370773403b3c945866bbf9dad9c8770e2cfe46
Akhil Sarikonda <akhilascoder@gmail.com>
2019-09-05 10:16:30 +0530

    added code

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
-import React from 'react';
-import {Grid} from '@material-UI/core';
+import React, { useState } from "react";
+import { Grid } from "@material-ui/core";
 
-import youtube from '../src/api/youtube';
+import { SearchBar, VideoList, VideoDetail } from "./components";
 
-class App extends React.Component{
-    render(){
-        return(
-            <Grid justify="center" container spacing={16}>
-                <Grid item xs={12}>
-                    <Grid container spacing={16}>
-                        <Grid item xs={12}>
-                            {/* {Search bar} */}
-                        </Grid>
+import youtube from "./api/youtube";
 
-                        <Grid item xs={8}>
-                            {/* {Video details} */}
-                        </Grid>
+export default () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
-                        <Grid item xs={4}>
-                            {/* {video list} */}
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </Grid>
-        )
-    }
-};
+  return (
+    <Grid style={{ justifyContent: "center" }} container spacing={10}>
+      <Grid item xs={11}>
+        <Grid container spacing={10}>
+          <Grid item xs={12}>
+            <SearchBar onSubmit={handleSubmit} />
+          </Grid>
+          <Grid item xs={8}>
+            <VideoDetail video={selectedVideo} />
+          </Grid>
+          <Grid item xs={4}>
+            <VideoList videos={videos} onVideoSelect={setSelectedVideo} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 
+  async function handleSubmit(searchTerm) {
+    const { data: { items: videos } } = await youtube.get("search", {
+      params: {
+        part: "snippet",
+        maxResults: 5,
+        key: process.env.REACT_APP_API_KEY,
+        q: searchTerm,
+      }
+    });
 
-export default App;
-
-
+    setVideos(videos);
+    setSelectedVideo(videos[0]);
+  }
+}
